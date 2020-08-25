337616b4d162a7518cecd8d0f3a682f0a887bfa0
Edgar Latorre <me@edgarlatorre.com.br>
2020-08-25 15:26:23 +0200

    Add support to localization

--- test/tobaccos_club_web/live/page_live_test.exs
+++ test/tobaccos_club_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule TobaccosClubWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Tobaccos Club"
+    assert disconnected_html =~ "Tobacco"
     assert render(page_live) =~ "Brands"
   end
 end
