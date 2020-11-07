e2a4025d59ae2e9c18af7fa07a7a92fa15218931
Edgar Latorre <me@edgarlatorre.com.br>
2020-11-07 23:19:36 +0100

    Add function to return filter by name

--- lib/tobaccos_club/pipes/filter.ex
+++ lib/tobaccos_club/pipes/filter.ex
@@ -1,12 +1,23 @@
 defmodule TobaccosClub.Pipes.Filter do
-  defstruct [:blend_type_ids, :countries, :search_text, :starts_with]
+  defstruct [:blend_type_ids, :countries, :cut_ids, :search_text, :starts_with]
 
   def build do
     %TobaccosClub.Pipes.Filter{
       blend_type_ids: [],
       countries: [],
+      cut_ids: [],
       search_text: "",
       starts_with: ""
     }
+  end
+
+  def filter_by_name(filter, name) do
+    case name do
+      "cut_ids" -> filter.cut_ids
+      "blend_type_ids" -> filter.blend_type_ids
+      "countries" -> filter.countries
+      "search_text" -> filter.search_text
+      "starts_with" -> filter.starts_with
+    end
   end
 end
--- test/tobaccos_club/pipes/filter_test.exs
+++ test/tobaccos_club/pipes/filter_test.exs
@@ -4,8 +4,38 @@ defmodule TobaccosClub.Pipes.FilterTest do
 
   describe "Filter" do
     test "build empty filter" do
-      assert %{starts_with: "", search_text: "", blend_type_ids: [], countries: []} =
+      assert %{starts_with: "", search_text: "", blend_type_ids: [], countries: [], cut_ids: []} =
                Filter.build()
+    end
+  end
+
+  describe "filter_by_name" do
+    @filter %Filter{
+      starts_with: "A",
+      search_text: "peterson",
+      blend_type_ids: [1, 2],
+      countries: ["BR"],
+      cut_ids: [3, 4]
+    }
+
+    test "cut_ids" do
+      assert [3, 4] == Filter.filter_by_name(@filter, "cut_ids")
+    end
+
+    test "blend_type_ids" do
+      assert [1, 2] == Filter.filter_by_name(@filter, "blend_type_ids")
+    end
+
+    test "countries" do
+      assert ["BR"] == Filter.filter_by_name(@filter, "countries")
+    end
+
+    test "search_text" do
+      assert "peterson" == Filter.filter_by_name(@filter, "search_text")
+    end
+
+    test "starts_with" do
+      assert "A" == Filter.filter_by_name(@filter, "starts_with")
     end
   end
 end
