d916b9473965803955432c73a51d25fddf20778a
Edgar Latorre <me@edgarlatorre.com.br>
2020-12-15 12:26:27 +0100

    Add function to list countries by ids

--- lib/tobaccos_club/pipes.ex
+++ lib/tobaccos_club/pipes.ex
@@ -26,7 +26,7 @@ defmodule TobaccosClub.Pipes do
 
   ## Examples
 
-      iex> list_cuts([1, 2, 3])
+      iex> list_cuts_by_ids([1, 2, 3])
       [%Cut{id: 1}, ...]
 
   """
@@ -132,6 +132,21 @@ defmodule TobaccosClub.Pipes do
   """
   def list_countries do
     Repo.all(Country)
+  end
+
+  @doc """
+  Returns the list of countries by ids.
+
+  ## Examples
+
+      iex> list_countries_by_ids([1, 2, 3])
+      [%Cut{id: 1}, ...]
+
+  """
+  def list_countries_by_ids(ids) do
+    query = from(b in Country, where: b.id in ^ids)
+
+    Repo.all(query)
   end
 
   @doc """
