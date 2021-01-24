73d292004109460bf0f6650d66d049220cc07747
Edgar Latorre <me@edgarlatorre.com.br>
2021-01-24 09:18:09 +0100

    Add modal component

--- lib/tobaccos_club_web/live/live_helpers.ex
+++ lib/tobaccos_club_web/live/live_helpers.ex
@@ -43,4 +43,10 @@ defmodule TobaccosClubWeb.LiveHelpers do
     </div>
     """
   end
+
+  def live_modal(socket, component, opts) do
+    path = Keyword.fetch!(opts, :return_to)
+    modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
+    live_component(socket, TobaccosClubWeb.ModalComponent, modal_opts)
+  end
 end
