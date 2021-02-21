43bd8976b3ea4656a349bd63f810c30b40cefcb4
Edgar Latorre <me@edgarlatorre.com.br>
2021-02-21 21:19:28 +0100

    use templates for user email

--- lib/tobaccos_club/accounts/user_email.ex
+++ lib/tobaccos_club/accounts/user_email.ex
@@ -1,61 +1,32 @@
 defmodule TobaccosClub.Accounts.UserEmail do
+  use Phoenix.Swoosh,
+    view: TobaccosClubWeb.EmailView,
+    layout: {TobaccosClubWeb.LayoutView, :email}
+
   import Swoosh.Email
   import TobaccosClubWeb.Gettext
 
   def confirmation_instructions(user, url) do
-    msg = """
-    #{dgettext("users", "Hi %{email}", email: user.email)},
-
-    #{dgettext("users", "You can confirm your account by visiting the URL below:")}
-
-    #{url}
-
-    #{dgettext("users", "If you didn't create an account with us, please ignore this.")}
-    """
-
     new()
     |> to({user.email, user.email})
     |> from({gettext("Tobacco's Club"), "noreply@tabacos.club"})
     |> subject(dgettext("users", "Instructions to account confirmation"))
-    |> html_body(msg)
-    |> text_body(msg)
+    |> render_body("accounts/account_confirmation.html", %{user: user, url: url})
   end
 
   def change_email_instructions(user, url) do
-    msg = """
-    #{dgettext("users", "Hi %{email}", email: user.email)},
-
-    #{dgettext("users", "You can change your email by visiting the URL below:")}
-
-    #{url}
-
-    #{dgettext("users", "If you didn't request this change, please ignore this.")}
-    """
-
     new()
     |> to({user.email, user.email})
     |> from({gettext("Tobacco's Club"), "noreply@tabacos.club"})
     |> subject(dgettext("users", "Change email instructions"))
-    |> html_body(msg)
-    |> text_body(msg)
+    |> render_body("accounts/change_email_confirmation.html", %{user: user, url: url})
   end
 
   def reset_password_instructions(user, url) do
-    msg = """
-    #{dgettext("users", "Hi %{email}", email: user.email)},
-
-    #{dgettext("users", "You can reset your password by visiting the URL below:")}
-
-    #{url}
-
-    #{dgettext("users", "If you didn't request this change, please ignore this.")}
-    """
-
     new()
     |> to({user.email, user.email})
     |> from({gettext("Tobacco's Club"), "noreply@tabacos.club"})
     |> subject(dgettext("users", "Reset password instructions"))
-    |> html_body(msg)
-    |> text_body(msg)
+    |> render_body("accounts/reset_password.html", %{user: user, url: url})
   end
 end
