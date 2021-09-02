c6b9227f9be01201a304ad37dcc11361b32a8653
Edgar Latorre <me@edgarlatorre.com.br>
2021-09-02 18:02:46 +0200

    chore: bump version to 0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TobaccosClub.MixProject do
   def project do
     [
       app: :tobaccos_club,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
