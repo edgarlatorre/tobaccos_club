1f9d700044f8bac98a9b0acd8b6be44b167c4959
Edgar Latorre <me@edgarlatorre.com.br>
2022-01-03 23:45:51 +0100

    improvement: create svg_image_tag

--- lib/tobaccos_club_web/views/layout_view.ex
+++ lib/tobaccos_club_web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule TobaccosClubWeb.LayoutView do
   use TobaccosClubWeb, :view
+
+  def svg_image_tag(name) do
+    content_tag(
+      :span,
+      raw(File.read!("assets/static/images/#{name}.svg")),
+      class: "svg-#{name}"
+    )
+  end
 end
