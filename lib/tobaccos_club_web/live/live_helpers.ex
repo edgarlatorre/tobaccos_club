5ae0a87a7ad66a2562b23b7efbd3bc047c135f98
Edgar Latorre <me@edgarlatorre.com.br>
2022-01-04 12:14:19 +0100

    improvement: Refactoring in the index page
    * change table to be fixed
    * change svg tags to use svg files

--- lib/tobaccos_club_web/live/live_helpers.ex
+++ lib/tobaccos_club_web/live/live_helpers.ex
@@ -28,14 +28,10 @@ defmodule TobaccosClubWeb.LiveHelpers do
         <div>
           <nav class="relative z-0 inline-flex">
             <%= live_patch to: Routes.blends_index_path(@socket, @route, page: @pagination_info.page_number - 1), class: "relative inline-flex items-center px-2 py-2 rounded-r-md border-0 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" do %>
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-            </svg>
+              <%= svg_image_tag("previous-icon") %>
             <% end %>
             <%= live_patch to: Routes.blends_index_path(@socket, @route, page: @pagination_info.page_number + 1), class: "relative inline-flex items-center px-2 py-2 rounded-r-md border-0 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" do %>
-              <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-              </svg>
+              <%= svg_image_tag("next-icon") %>
             <% end %>
           </nav>
         </div>
@@ -48,5 +44,9 @@ defmodule TobaccosClubWeb.LiveHelpers do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
     live_component(TobaccosClubWeb.ModalComponent, modal_opts)
+  end
+
+  def svg_image_tag(name) do
+    TobaccosClubWeb.LayoutView.svg_image_tag(name)
   end
 end
