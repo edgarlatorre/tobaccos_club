1c396526a91172a0663da745bf79a65f673f4071
Edgar Latorre <me@edgarlatorre.com.br>
2022-01-05 03:02:38 +0100

    fix: svg tag

--- lib/tobaccos_club_web/views/layout_view.ex
+++ lib/tobaccos_club_web/views/layout_view.ex
@@ -2,9 +2,11 @@ defmodule TobaccosClubWeb.LayoutView do
   use TobaccosClubWeb, :view
 
   def svg_image_tag(name) do
+    file_path = Path.join(:code.priv_dir(:tobaccos_club), "/static/assets/images/#{name}.svg")
+
     content_tag(
       :span,
-      raw(File.read!("assets/static/images/#{name}.svg")),
+      raw(File.read!(file_path)),
       class: "svg-#{name}"
     )
   end
